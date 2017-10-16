495e0d7462aef5a9a7ef1310fcef92b77c5dc107
tuka <tomascode108@gmail.com>
2017-10-16 18:16:36 +0200

    intial commit3

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use((req, res, next) => {
     var now = new Date().toString();
     var log = `${now}:${req.method} ${req.url}`;
     console.log(log);
-    fs.appendFile('server_log', log + '\n', (err) => {
+    fs.appendFile('server.log', log + '\n', (err) => {
         if (err) {
-            console.log('unable to append to server_log.');
+            console.log('unable to append to server.log.');
         };
     });
     next();
